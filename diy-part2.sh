c2cbb6898e457237ff8b5b96f2eb8e8b56f2672a
Armin <70259392+Armin-0401@users.noreply.github.com>
2021-11-27 21:47:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认wifi名称ssid为404.not.found.lab
+sed -i 's/ssid=OpenWrt/ssid=404.not.found.lab/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
