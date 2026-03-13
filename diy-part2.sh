3fa00139acc7309d9a70b0482a4545575ac0bce4
Armin <70259392+Armin-0401@users.noreply.github.com>
2026-03-13 02:54:03 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ rm -rf package/lean/luci-theme-netgear
 rm -rf package/lean/luci-theme-ifit
 
 ### 修改主题文件
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -26,7 +26,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Mi-R4A'' package/lean/default-settings/files/zzz-default-settings
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.31/g' package/base-files/files/bin/config_generate
 
 # 修改默认wifi名称ssid为404.not.found.lab
-sed -i 's/ssid=OpenWrt/ssid=404.not.found.lab/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/ssid=OpenWrt/ssid=404.not.found.lab/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
