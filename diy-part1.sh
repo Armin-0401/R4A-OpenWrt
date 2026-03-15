13db0082ce9c796f239cfb248ca5435b0e28ec04
Armin <70259392+Armin-0401@users.noreply.github.com>
2026-03-15 09:16:36 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,26 +25,19 @@ rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
 git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
 ./scripts/feeds install -a
 
+# 2. 修改 R4A 閃存佈局 (適配 Breed 直刷)
+# 採用區塊匹配，不再依賴不穩定的行號
+DTS_FILE="target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi"
+if [ -f "$DTS_FILE" ]; then
+    # 刪除舊的 spi0 節點並插入 Breed 16MB 佈局
+    sed -i '/&spi0 {/,/&pcie {/ { /&pcie {/!d }' "$DTS_FILE"
+    sed -i '/&pcie {/i &spi0 {\n\tstatus = "okay";\n\tflash@0 {\n\t\tcompatible = "jedec,spi-nor";\n\t\treg = <0>;\n\t\tspi-max-frequency = <50000000>;\n\t\tpartitions {\n\t\t\tcompatible = "fixed-partitions";\n\t\t\t#address-cells = <1>;\n\t\t\t#size-cells = <1>;\n\t\t\tpartition@0 { label = "u-boot"; reg = <0x0 0x30000>; read-only; };\n\t\t\tpartition@30000 { label = "u-boot-env"; reg = <0x30000 0x10000>; read-only; };\n\t\t\tfactory: partition@40000 { label = "factory"; reg = <0x40000 0x10000>; read-only; };\n\t\t\tpartition@50000 { compatible = "denx,uimage"; label = "firmware"; reg = <0x50000 0xfb0000>; };\n\t\t};\n\t};\n};' "$DTS_FILE"
+    echo "DTS Breed Patch applied."
+fi
 
+# 3. 修復 IMAGE_SIZE (16MB)
+sed -i 's/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/' target/linux/ramips/image/mt7621.mk
 
-
-### 修改为R4A千兆版Breed直刷版
-## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
-## 1.修改 mt7621_xiaomi_mir3g-v2.dts
-export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
-export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
-export shanchu2=$(expr $shanchu2 - 1)
-export shanchu2=$(echo $shanchu2"d")
-sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
-echo "" >> youhua.txt
-echo "" >> youhua.txt
-export shanchu1=$(expr $shanchu1 - 1)
-export shanchu1=$(echo $shanchu1"r")
-sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-rm -rf youhua.txt
-## 2.修改mt7621.mk
-export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
-export imsize1=$(expr $imsize1 + 2)
-export imsize1=$(echo $imsize1"s")
-sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
+# 4. 強制升級 Golang 以支持 HomeProxy
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 22.x feeds/packages/lang/golang
