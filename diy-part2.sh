2793ebb95b7d7e567b567a8c5b7b83eda17fb579
Armin <70259392+Armin-0401@users.noreply.github.com>
2026-03-15 07:55:42 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,10 +16,7 @@ rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
 rm -rf package/lean/luci-theme-ifit
 
-### 修改主题文件
-# git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+# 預設主題
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名字，把Xiaomi-R4A修改你喜欢的就行（不能纯数字或者使用中文）
@@ -29,4 +26,4 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='Mi-R4A'' packag
 sed -i 's/192.168.1.1/192.168.1.31/g' package/base-files/files/bin/config_generate
 
 # 修改默认wifi名称ssid为404.not.found.lab
-# sed -i 's/ssid=OpenWrt/ssid=404.not.found.lab/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ssid=OpenWrt/ssid=404.not.found.lab/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
