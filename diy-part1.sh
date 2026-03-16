02dc35fa85e0fb528d2afec77fe341187cab955a
Armin <70259392+Armin-0401@users.noreply.github.com>
2026-03-16 06:25:14 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,6 @@
 
 # 1. 添加第三方插件源
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
-echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >>feeds.conf.default
+# echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
