56075c306b9592c65026e1c5c7f6db59acc22ab6
Armin <70259392+Armin-0401@users.noreply.github.com>
2026-03-16 02:53:03 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,8 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# 物理刪除報錯的 shadowsocksr-libev
-rm -rf feeds/small/shadowsocksr-libev
 #删除原默认主题
 # rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-theme-bootstrap
@@ -21,7 +19,7 @@ rm -rf package/lean/luci-theme-ifit
 # 預設主題
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 修改主机名字，把Xiaomi-R4A修改你喜欢的就行（不能纯数字或者使用中文）
+# 修改主机名字
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Mi-R4A'' package/lean/default-settings/files/zzz-default-settings
 
 # Modify default IP
